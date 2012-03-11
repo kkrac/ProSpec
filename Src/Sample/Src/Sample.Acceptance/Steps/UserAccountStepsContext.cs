2029cbe1e808e1354f896bf912a3245081d7b2d6
kevin k <kkrac78@gmail.com>
2012-03-10 22:51:52 -0300

    moving helpers out from removed class useraccountstepsutils

--- Src/Sample/Src/Sample.Acceptance/Steps/UserAccountStepsContext.cs
+++ Src/Sample/Src/Sample.Acceptance/Steps/UserAccountStepsContext.cs
@@ -1,16 +1,84 @@
 ﻿using System;
-using TwoK.Core.IoC;
-using TwoK.Core.Security.Encryption;
-using Sample.Acceptance.Steps.UI;
-using Sample.Acceptance.Support;
+using Sample.Acceptance.Support.Queries;
 using Sample.Data.Access;
+using Sample.Data.Access.Queries;
 using Sample.Domain.Model.User;
 using TechTalk.SpecFlow;
+using TwoK.Core.IoC;
+using TwoK.Core.Security.Encryption;
 
 namespace Sample.Acceptance.Steps
 {
-    public class UserAccountStepsContext : SampleStepsContext
+    public abstract class UserAccountStepsContext : SampleStepsContext
     {
+        protected void DeletePeople()
+        {
+            IPersons_DeleteAll deleteUsers = IoCProvider.Resolve<IPersons_DeleteAll>();
+            deleteUsers.Execute();
+        }
+
+        protected void DeleteUsers()
+        {
+            IUsers_DeleteAll deleteUsers = IoCProvider.Resolve<IUsers_DeleteAll>();
+            deleteUsers.Execute();
+        }
+
+        protected void DeleteTemporaryUsers()
+        {
+            IUsers_Temp_DeleteAll deleteTemporaryUsers = IoCProvider.Resolve<IUsers_Temp_DeleteAll>();
+            deleteTemporaryUsers.Execute();
+        }
+
+        protected AuthenticatedUser GetUser(string userId)
+        {
+            IUser_SelectById selectUserById = IoCProvider.Resolve<IUser_SelectById>();
+
+            return selectUserById.Execute(userId);
+        }
+
+        protected bool IsUserActive(string userId)
+        {
+            IUser_IsActive userIsActive = IoCProvider.Resolve<IUser_IsActive>();
+
+            return userIsActive.Execute(userId);
+        }
+
+        protected void CreateAccount(string userId, string password, string token, string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            ISampleUser user = new AuthenticatedUser(userId, firstName, lastName, dateOfBirth, email);
+
+            IPerson_Insert insertPerson = IoCProvider.Resolve<IPerson_Insert>();
+
+            int personId = (int)insertPerson.Execute
+                                (
+                                    user.FirstName,
+                                    user.LastName,
+                                    user.Email.ToString(),
+                                    user.DateOfBirth,
+                                    user.Id,
+                                    DateTime.Now
+                                );
+
+            IUser_Insert insertUser = IoCProvider.Resolve<IUser_Insert>();
+
+            insertUser.Execute
+            (
+                user.Id,
+                personId,
+                password,
+                false,
+                user.Id,
+                DateTime.Now
+            );
+
+            IUser_Temp_Insert insertTemporaryAccount = IoCProvider.Resolve<IUser_Temp_Insert>();
+            insertTemporaryAccount.Execute
+            (
+                userId,
+                token
+            );
+        }
+
         [Given(@"the following temporary accounts exist")]
         public void Given_The_Following_Temporary_Account_Exists(Table table)
         {
@@ -21,7 +89,7 @@ namespace Sample.Acceptance.Steps
                 string userName = row["UserName"];
                 string token = row["Token"];
 
-                UserAccountStepsUtils.CreateAccount(userName, "P@ssword1", token, "john" + i, "doe", "john" + i + "@doe.com", DateTime.Now);
+                CreateAccount(userName, "P@ssword1", token, "john" + i, "doe", "john" + i + "@doe.com", DateTime.Now);
             }
         }
 
