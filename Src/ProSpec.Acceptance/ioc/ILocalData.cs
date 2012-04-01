8be389415a42cef9d48704f409efd28b99e84f3a
kevin k <kkrac78@gmail.com>
2012-04-01 13:54:23 -0300

    added ioc container

---
+++ Src/ProSpec.Acceptance/ioc/ILocalData.cs
@@ -0,0 +1,9 @@
+﻿namespace ProSpec.Acceptance.IoC
+{
+    public interface ILocalData
+    {
+        object this[object key] { get; set; }
+        int Count { get; }
+        void Clear();
+    }
+}
---
+++ Src/ProSpec.Acceptance/ioc/Local.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Web;
+
+namespace ProSpec.Acceptance.IoC
+{
+    public static class Local
+    {
+        private static readonly ILocalData _data = new LocalData();
+
+        public static ILocalData Data
+        {
+            get { return _data; }
+        }
+
+        private class LocalData : ILocalData
+        {
+            [ThreadStatic]
+            private static Hashtable _localData;
+            private static readonly object LocalDataHashtableKey = new object();
+
+            private static Hashtable LocalHashtable
+            {
+                get 
+                {
+                    if (!RunningInWeb)
+                    {
+                        if (_localData == null)
+                            _localData = new Hashtable();
+                        return _localData;
+                    }
+                    else
+                    {
+                        var web_hashtable = HttpContext.Current.Items[LocalDataHashtableKey] as Hashtable;
+                        if (web_hashtable == null)
+                        {
+                            web_hashtable = new Hashtable();
+                            HttpContext.Current.Items[LocalDataHashtableKey] = web_hashtable;
+                        }
+                        return web_hashtable;
+                    }
+                }
+            }
+
+            public object this[object key]
+            {
+                get { return LocalHashtable[key]; }
+                set { LocalHashtable[key] = value; }
+            }
+
+            public int Count
+            {
+                get { return LocalHashtable.Count; }
+            }
+
+            public void Clear()
+            {
+                LocalHashtable.Clear();
+            }
+
+            public static bool RunningInWeb
+            {
+                get { return HttpContext.Current != null; }
+            }
+        }
+    }
+}
