c0d58a75c28a282f05085dfb5259f86271036f9f
kevin k <kkrac78@gmail.com>
2012-04-01 18:53:09 -0300

    removed ioc

--- Src/ProSpec.Acceptance/IoC/ILocalData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProSpec.Acceptance.IoC
-{
-    public interface ILocalData
-    {
-        object this[object key] { get; set; }
-        int Count { get; }
-        void Clear();
-    }
-}
