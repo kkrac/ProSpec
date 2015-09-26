cdb2cfef2384c1c509a79d95ef1a13de36d86f8f
kevin k <kkrac78@gmail.com>
2015-09-26 00:41:16 -0300

    pending changes...

---
+++ Src/ProSpec.WatiNIntegration/TextFieldExtended.cs
@@ -0,0 +1,37 @@
+﻿using WatiN.Core;
+using WatiN.Core.Native;
+
+namespace ProSpec.WatiNIntegration
+{
+    [ElementTag("input", InputType = "text", Index = 0)]
+    [ElementTag("input", InputType = "password", Index = 1)]
+    [ElementTag("input", InputType = "textarea", Index = 2)]
+    [ElementTag("input", InputType = "hidden", Index = 3)]
+    [ElementTag("textarea", Index = 4)]
+    [ElementTag("input", InputType = "email", Index = 5)]
+    [ElementTag("input", InputType = "url", Index = 6)]
+    [ElementTag("input", InputType = "number", Index = 7)]
+    [ElementTag("input", InputType = "range", Index = 8)]
+    [ElementTag("input", InputType = "search", Index = 9)]
+    [ElementTag("input", InputType = "color", Index = 10)]
+    [ElementTag("input", InputType = "date", Index = 11)]
+    [ElementTag("input", InputType = "datetime", Index = 12)]    
+    public class TextFieldExtended : TextField
+    {
+        public TextFieldExtended(DomContainer domContainer, INativeElement element)
+            : base(domContainer, element)
+        {
+        }
+
+        public TextFieldExtended(DomContainer domContainer, ElementFinder finder)
+            : base(domContainer, finder)
+        {
+        }
+
+        public static void Register()
+        {
+            var typeToRegister = typeof(TextFieldExtended);
+            ElementFactory.RegisterElementType(typeToRegister);
+        }
+    }
+}
---
+++ Src/ProSpec.WatiNIntegration/WatiNFireFoxBrowser.cs
@@ -0,0 +1,82 @@
+﻿namespace ProSpec.WatiNIntegration
+{
+    using System.Diagnostics;
+    using System.Net;
+    using ProSpec.Core.UI.Web;
+    using WatiN.Core;
+    using Page = ProSpec.Core.UI.Web.Page;
+
+    /// <summary>
+    /// Interface to the Internet Explorer WatiN browser object to access elements on a page.
+    /// </summary>
+    public class WatiNFireFoxBrowser : FireFox, IBrowser
+    {
+        public WatiNFireFoxBrowser()
+        {
+            //observer = new NavigationObserver(this);
+        }
+
+        public override void Close()
+        {
+            base.Close();
+
+            Process[] processes = Process.GetProcessesByName("firefox");
+
+            foreach (Process process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public bool IsOnPage(Page page)
+        {
+            return Url.Equals(page.RawUrl);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public HttpStatusCode Status
+        {
+            get;
+            private set;
+        }
+
+        //private NavigationObserver observer;
+
+        //private class NavigationObserver
+        //{
+        //    private WatiNBrowser browser;
+        //    private bool errorOccured;
+
+        //    public NavigationObserver(WatiNFireFoxBrowser browser)
+        //    {
+        //        this.browser = browser;
+        //        InternetExplorer internetExplorer = (FireFox)browser.fi;
+        //        internetExplorer.NavigateError += IeNavigateError;
+        //        internetExplorer.NavigateComplete2 += internetExplorer_NavigateComplete2;
+        //    }
+
+        //    private void internetExplorer_NavigateComplete2(object pDisp, ref object URL)
+        //    {
+        //        if (!errorOccured)
+        //        {
+        //            browser.Status = HttpStatusCode.OK;
+        //        }
+
+        //        errorOccured = false;
+        //    }
+
+        //    private void IeNavigateError(object pDisp, ref object URL, ref object Frame, ref object StatusCode, ref bool Cancel)
+        //    {
+        //        errorOccured = true;
+
+        //        browser.Status = (HttpStatusCode)StatusCode;
+        //    }
+    }
+}
